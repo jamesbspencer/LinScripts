6f59e1092436f63556adae76969858710d5fc417
James Spencer <trik57r@users.noreply.github.com>
2018-12-07 08:06:48 -0500

    Update create_vm.sh

--- create_vm.sh
+++ create_vm.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+##Script to create a VM with virt-install.
+##Requires virt-install to be installed. 
+##Created by: James Spencer
+
 
 name='repo'             #VM Name
 type='linux'            #OS type linux or windows
 variant='centos7.0'     #OS Variant win2k8,rhel7,centos7.0
 mem='2048'              #Memory in MB
 cpu='1'                 #Number of CPUs
-dsize='100'             #Disk Size in GB
+dsize='25G'             #Disk Size in GB
 dpath='/data1/data'     #Disk Location on NFS Mount
 cdiso='CentOS-7-x86_64-DVD-1804.iso'    #CD ISO Name
 cdpath='/data1/iso'     #CD ISO Path on NFS Mount
 net='bridge0'
 
+#Create the disk image because virt-install doesn't seem to thin provision
+qemu-img create -f qcow2 $dpath/$name.qcow2 $dsize
+
+#Create the VM and start the installer
 virt-install \
 --virt-type kvm \
 --name $name \
@@ -18,7 +26,7 @@ virt-install \
 --vcpus $cpu \
 --os-type $type \
 --os-variant $variant \
---disk path=$dpath/$name.qcow2,size=$dsize,sparce=true,format=qcow2 \
+--disk path=$dpath/$name.qcow2 \
 --cdrom $cdpath/$cdiso \
 --network bridge=$net \
 --graphics vnc \
